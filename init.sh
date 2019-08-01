db601a144e53290532f2fe01220aa66d44f9afa9
cheater <angryshikamaru@gmail.com>
2019-08-01 12:16:32 +0300

    correct init.sh: include gunicorn

--- init.sh
+++ init.sh
@@ -4,27 +4,46 @@ WORK_DIR="/home/box"
 PROJ_DIR="$WORK_DIR/web"
 
 
+init_nginx()
+{
+	# include our settings to nginx main config
+	sudo ln -sf $PROJ_DIR/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
+	# delete default location settings
+	sudo rm -rf /etc/nginx/sites-enabled/default
+	# start/restart nginx
+	sudo /etc/init.d/nginx restart
+}
+
+init_gunicorn()
+{
+	# delete example settings
+	sudo rm /etc/gunicorn.d/*
+	# include setting for hello.py to gunicorn
+	sudo ln -sf $PROJ_DIR/etc/hello.conf /etc/gunicorn.d/hello.conf
+	# start gunicorn
+	sudo /etc/init.d/gunicorn start
+}
+
+
 ################
 ####  MAIN  ####
 ################
 case "$1" in
 	init)
-		# include our settings to nginx main config
-		sudo ln -sf $PROJ_DIR/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
-		# delete default location settings
-		sudo rm -rf /etc/nginx/sites-enabled/default
-		# start nginx
-		sudo /etc/init.d/nginx stop
-		sudo /etc/init.d/nginx start
+		init_nginx
+		init_gunicorn
 	;;
 	start)
 		sudo /etc/init.d/nginx start
+		sudo /etc/init.d/gunicorn start
 	;;
 	restart)
 		sudo /etc/init.d/nginx restart
+		sudo /etc/init.d/restart restart
 	;;
 	stop)
 		sudo /etc/init.d/nginx stop
+		sudo /etc/init.d/gunicorn stop
 	;;
 	*)
 		echo "Usage: $0 {init|start|stop|restart}" >&2
