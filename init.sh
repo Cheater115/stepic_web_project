1e09a5044351560b7b6088e8c2dea03533f44c43
cheater <angryshikamaru@gmail.com>
2019-08-01 14:58:14 +0300

    correct init.sh to start mysql

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ WORK_DIR="/home/box"
 PROJ_DIR="$WORK_DIR/web"
 DJ_PROJ_DIR="$PROJ_DIR/ask"
 DJ_APP_NAME="qa"
+DB_NAME="ask"
+DB_USER_NAME="box"
 
 
 init_nginx()
@@ -28,6 +30,18 @@ init_gunicorn()
 	sudo /etc/init.d/gunicorn restart
 }
 
+init_mysql()
+{
+	# Delete old settings
+	sudo rm /etc/mysql/conf.d/mysql.cnf
+	# Start MySQL
+	sudo /etc/init.d/mysql start
+	# Create database
+	sudo mysql -u root -e "create database if not exists $DB_NAME;"
+	# Create user with privileges to manage this database
+	sudo mysql -u root -e "grant all privileges on $DB_NAME.* to '$DB_USER_NAME'@'localhost' with grant option;"
+}
+
 
 ################
 ####  MAIN  ####
@@ -36,18 +50,22 @@ case "$1" in
 	init)
 		init_nginx
 		init_gunicorn
+		init_mysql
 	;;
 	start)
 		sudo /etc/init.d/nginx start
 		sudo /etc/init.d/gunicorn start
+		sudo /etc/init.d/mysql start
 	;;
 	restart)
 		sudo /etc/init.d/nginx restart
 		sudo /etc/init.d/restart restart
+		sudo /etc/init.d/mysql restart
 	;;
 	stop)
 		sudo /etc/init.d/nginx stop
 		sudo /etc/init.d/gunicorn stop
+		sudo /etc/init.d/mysql stop
 	;;
 	*)
 		echo "Usage: $0 {init|start|stop|restart}" >&2
