2cfa6ed8cfba9f6777f6fcd8c3e5a3cc32ca77b9
cheater <angryshikamaru@gmail.com>
2019-08-01 11:07:40 +0300

    create script for init|start|restart|stop server

---
+++ init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+WORK_DIR="/home/box"
+PROJ_NAME="$WORK_DIR/web"
+
+
+################
+####  MAIN  ####
+################
+case "$1" in
+	init)
+		# include our settings to nginx main config
+		sudo ln -sf $PROJ_DIR/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
+		# delete default location settings
+		sudo rm -rf /etc/nginx/sites-enabled/default
+		# start nginx
+		sudo /etc/init.d/nginx start
+	;;
+	start)
+		sudo /etc/init.d/nginx start
+	;;
+	restart)
+		sudo /etc/init.d/nginx restart
+	;;
+	stop)
+		sudo /etc/init.d/nginx stop
+	;;
+	*)
+		echo "Usage: $0 {init|start|stop|restart}" >&2
+		exit 1
+	;;
+esac
