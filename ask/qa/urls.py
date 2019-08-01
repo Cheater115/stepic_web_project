a013d3797acd9706d9f866a5a65f7806aed3b583
cheater <angryshikamaru@gmail.com>
2019-08-01 19:15:26 +0300

    correct urls.py: add logout, login, signup

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^question/(?P<id>\d+)/$', views.question_details, name = 'question'),
-    url(r'^login/$', views.test, name = 'login'),
-    url(r'^signup/$', views.test, name = 'signup'),
+    url(r'^login/$', views.user_login, name = 'login'),
+    url(r'^logout/$', views.user_logout, name = 'logout'),
+    url(r'^signup/$', views.user_signup, name = 'signup'),
     url(r'^ask/$', views.ask_question, name = 'ask'),
     url(r'^popular/$', views.popular_questions, name = 'popular'),
     url(r'^new/$', views.new_questions, name = 'new'),
