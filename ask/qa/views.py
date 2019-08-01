429832fa05a624028026949e7caadc283f945f63
cheater <angryshikamaru@gmail.com>
2019-08-01 18:02:27 +0300

    correct vievs: import HtppResponseRedirect

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Question, Answer
