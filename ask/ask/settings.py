1153dbf34caa990f86e8ce845707ff373b1d8c41
cheater <angryshikamaru@gmail.com>
2019-08-01 13:40:45 +0300

    correct errors in routing

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v-08++yknyd-odgs_l&4xn7e_n=r!m%ilw9l&*)@u)!w35*#v)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
