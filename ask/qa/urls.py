fbc9f97054749edfde77a5fbc1f7f4f9c6a57c05
cheater <angryshikamaru@gmail.com>
2019-08-01 16:07:48 +0300

    configurate routing for new views

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^question/\d+/$', views.test, name = 'question'),
+    url(r'^question/(?P<question_id>\d+)/$', views.question_details, name = 'question'),
     url(r'^login/$', views.test, name = 'login'),
     url(r'^signup/$', views.test, name = 'signup'),
     url(r'^ask/$', views.test, name = 'ask'),
-    url(r'^popular/$', views.test, name = 'popular'),
-    url(r'^new/$', views.test, name = 'new'),
-    url(r'^$', views.test, name = 'main'),
+    url(r'^popular/$', views.popular_questions, name = 'popular'),
+    url(r'^new/$', views.new_questions, name = 'new'),
+    url(r'^$', views.new_questions, name = 'main'),
 ]
